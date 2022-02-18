7d2fbbe0a3abebdaf200f1d86ff0503add005220
h1lary <krippakken@gmail.com>
2022-02-18 21:07:59 +0200

    lab1 done

---
+++ task1.cpp
@@ -0,0 +1,179 @@
+﻿#include <iostream>
+#include <fstream>
+
+int main()
+{
+    FILE* fp;
+    int i = 0, j = 0, countOfWords = 0, maxWordLength = 0;;
+    char current;
+
+    fopen_s(&fp, "1.txt", "rb");
+    if (fp == NULL) return 0;
+
+    char buffer[10000];
+    int bufferLength = 0;
+
+startReading:
+    current = fgetc(fp);
+    if (current == EOF)
+        goto endReading;
+
+    if (current != '\r' && current != '\n')
+    {
+        if ((current == ' ' && bufferLength != 0 && buffer[bufferLength - 1] != ' ') || current != ' ')
+        {
+            if (static_cast<int>(current) >= 65 && static_cast<int>(current) <= 90)
+                current = static_cast<char>(static_cast<int>(current) + 32);
+            buffer[bufferLength] = current;
+            bufferLength = bufferLength + 1;
+        }
+    }
+    else if (current == '\n')
+    {
+        buffer[bufferLength] = ' ';
+        bufferLength++;
+    }
+
+    goto startReading;
+endReading:
+    fclose(fp);
+
+    if (bufferLength == 0) return 0;
+    if (buffer[bufferLength - 1] == ' ') bufferLength--;
+
+    countOfWords++;
+    i = 0;
+checkSize:
+    if (i == bufferLength) goto endChecking;
+
+    if (buffer[i] == ' ') countOfWords++;
+
+    i++;
+    goto checkSize;
+endChecking:
+
+    int notSameWords = 0;
+    auto arrWords = new std::string[countOfWords];
+
+    i = 0;
+    j = 0;
+
+add:
+    if (i == bufferLength) goto endAdding;
+    if (buffer[i] == ' ' && i != 0)
+    {
+        j++;
+        i++;
+        goto add;
+    }
+
+    arrWords[j] += buffer[i];
+
+    i++;
+    goto add;
+endAdding:
+
+    int* count = new int[countOfWords];
+
+    i = 0;
+
+makingToZero:
+    if (i == countOfWords) goto endMakingToZero;
+    count[i] = 1;
+    i++;
+    goto makingToZero;
+endMakingToZero:
+
+    i = 0;
+    j = 0;
+
+deletingSmallWords:
+    if (i > countOfWords)
+        goto endDeletingSmallWords;
+    if (arrWords[i].size() < 3)
+        arrWords[i] = "";
+
+    i++;
+    goto deletingSmallWords;
+endDeletingSmallWords:
+
+
+    i = 0;
+    j = 0;
+count:
+
+    if (i >= countOfWords)
+    {
+        goto endCounting;
+    }
+    if (arrWords[i] != "")
+    {
+        int k = i + 1;
+    intern:
+        if (k >= countOfWords)
+            goto endIntern;
+        if (arrWords[i] == arrWords[k])
+        {
+            arrWords[k] = "";
+            count[k] = 0;
+            count[i]++;
+        }
+        k++;
+        goto intern;
+    endIntern:
+        int s;
+    }
+    i++;
+    goto count;
+
+
+
+endCounting:
+
+
+    //fopen_s(&fp, "output.txt", "w");
+    //fwrite()
+    std::string strTemp;
+    int temp;
+
+    i = 0;
+externalSort:
+    if (i >= countOfWords - 1) goto endSort;
+
+    j = 0;
+internalSort:
+    if (j >= countOfWords - i - 1) goto endInternal;
+        
+    if (count[j] < count[j + 1])
+    {
+        strTemp = arrWords[j];
+        temp = count[j];
+        arrWords[j] = arrWords[j + 1];
+        count[j] = count[j + 1];
+        arrWords[j + 1] = strTemp;
+        count[j + 1] = temp;
+    }
+        j++;
+        goto internalSort;
+    endInternal:
+
+    i++;
+    goto externalSort;
+endSort:
+
+    std::ofstream outFile("output.txt");
+
+    i = 0;
+output:
+    if (i >= countOfWords) goto endOutput;
+    if (arrWords[i] != "")
+    {
+        outFile << arrWords[i] << " - " << count[i] << std::endl;
+    }
+
+    i++;
+    goto output;
+endOutput:
+
+    outFile.close();
+}
---
+++ task2.cpp
@@ -0,0 +1,216 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+
+struct Word
+{
+    std::string word;
+    int page = 0;
+};
+
+int main()
+{
+    FILE* fp;
+    int i = 0, j = 0, countOfWords = 0;
+    char current;
+    int currPage = 1;
+    int countLinesProccessed = 0;
+
+
+    fopen_s(&fp, "2.txt", "rb");
+
+    if (fp == NULL) return 0;
+
+    int bufferLength = 0;
+    std::string currWord = "";
+    std::string allWords;
+    int bufferSize = 0;
+
+startReading:
+    current = fgetc(fp);
+    if (current == EOF)
+        goto endReading;
+
+    if (current != ' ' && current != ',' && current != '.' && current != '-' && current != ':' && current != '!' && current != '?'
+        && current != '\r' && current != '\n' && current != -30 && current != -128 && current != -99 && current != '_' && current != 'њ' && current != ';')
+    {
+        currWord += current;
+        bufferSize++;
+    }
+    else
+    {
+        if (currWord != "" && currWord != "1" && currWord != "0" && currWord != "2" && currWord != "3" && currWord != "4" && currWord != "5"
+            && currWord != "6" && currWord != "7" && currWord != "8" && currWord != "9" && current != -30 && current != -128 && current != -99 && current != '_' && current != 'њ')
+        {
+            allWords += currWord + " ";
+            countOfWords++;
+            bufferSize++;
+            currWord = "";
+        }
+    }
+
+    goto startReading;
+endReading:
+    fclose(fp);
+
+    int countOfSymbols = 0;
+    Word* arrayWords = new Word[countOfWords];
+    i = 0;
+
+    j = 0;
+beginAddingToStruct:
+    if (j >= allWords.size()) goto endAddingToStruct;
+
+
+    countOfSymbols++;
+    if (allWords[j] == ' ')
+    {
+        i++;
+        j++;
+        goto beginAddingToStruct;
+    }
+    if (static_cast<int>(allWords[j]) >= 65 && static_cast<int>(allWords[j]) <= 90)
+        allWords[j] = static_cast<char>(static_cast<int>(allWords[j]) + 32);
+
+    arrayWords[i].word += allWords[j];
+    arrayWords[i].page = static_cast<int>(countOfSymbols / 1800) + 1;
+
+
+    j++;
+    goto beginAddingToStruct;
+endAddingToStruct:
+
+
+    int countWord = 0;
+
+    i = 0;
+externalDeletingOver100:
+    if (i >= countOfWords) goto externalEndDeletingOver100;
+
+    countWord = 0;
+    currWord = arrayWords[i].word;
+
+    j = 0;
+internalCountSycle:
+    if (j >= countOfWords) goto endInternalCountSycle;
+
+
+
+    if (currWord == arrayWords[j].word)
+    {
+        countWord++;
+    }
+
+    j++;
+    goto internalCountSycle;
+endInternalCountSycle:
+
+
+    if (countWord > 100)
+    {
+        j = 0;
+    internalDeletingSycle:
+        if (j >= countOfWords) goto endInternalDeletingSycle;
+
+
+        if (arrayWords[i].word == currWord)
+        {
+            arrayWords[i].word = "";
+            arrayWords[i].page = 0;
+        }
+
+        j++;
+        goto internalDeletingSycle;
+
+    }
+endInternalDeletingSycle:
+    i++;
+    goto externalDeletingOver100;
+externalEndDeletingOver100:
+
+    i = 0;
+deleteWithSamePages:
+    if (i >= countOfWords) goto endDeleteWithSamePages;
+
+    currWord = arrayWords[i].word;
+    j = 0;
+internalDeleteWithSamePages:
+    if (j >= countOfWords) goto endInternalDeleteWithSamePages;
+
+    if (currWord == arrayWords[j].word && i != j)
+    {
+        if (arrayWords[i].page == arrayWords[j].page)
+        {
+            arrayWords[j].page = 0;
+            arrayWords[j].word = "";
+        }
+    }
+
+    j++;
+    goto internalDeleteWithSamePages;
+endInternalDeleteWithSamePages:
+
+    i++;
+    goto deleteWithSamePages;
+endDeleteWithSamePages:
+
+    std::string strTemp;
+    int temp;
+    i = 0;
+externalSort:
+    if (i >= countOfWords - 1) goto endSort;
+
+    j = 0;
+internalSort:
+    if (j >= countOfWords - i - 1) goto endInternal;
+
+    if (arrayWords[j].word > arrayWords[j + 1].word)
+    {
+        strTemp = arrayWords[j].word;
+        temp = arrayWords[j].page;
+        arrayWords[j].word = arrayWords[j + 1].word;
+        arrayWords[j].page = arrayWords[j + 1].page;
+        arrayWords[j + 1].word = strTemp;
+        arrayWords[j + 1].page = temp;
+    }
+    j++;
+    goto internalSort;
+endInternal:
+
+    i++;
+    goto externalSort;
+endSort:
+
+    std::ofstream outFile("output.txt");
+
+    i = 0;
+outputToFile:
+    if (i >= countOfWords) goto endOutputToFile;
+
+    currWord = arrayWords[i].word;
+
+    if (currWord != "")
+    {
+        outFile << currWord << " - ";
+
+        j = 0;
+    internalOutputToFile:
+        if (j >= countOfWords) goto internalEndOutputToFile;
+        if (currWord == arrayWords[j].word)
+        {
+            outFile << arrayWords[j].page << " ";
+            arrayWords[j].word = "";
+        }
+        j++;
+        goto internalOutputToFile;
+    internalEndOutputToFile:
+
+        outFile << std::endl;
+    }
+    i++;
+    goto outputToFile;
+endOutputToFile:
+
+    outFile.close();
+}
+
